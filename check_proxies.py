68465a2e1229676e52fd74c0e60385fe7af60e14
Vladimir <you@example.com>
2021-10-31 23:16:48 +0300

    parser_new_website_with_proxies

--- check_proxies.py
+++ check_proxies.py
@@ -6,7 +6,7 @@ URL = 'http://www.w3.org/TR/html4/strict.dtd'
 
 
 def get_proxy_html():
-    URL1 = 'https://free-proxy-list.net/'
+    URL1 = 'https://openproxy.space/ru/list/890-w2mSkt'
     response = requests.get(url=URL1)
     return response.text
 
@@ -15,35 +15,38 @@ def parse_proxies(resp):
     """Parse web site with free proxies."""
     proxies_list = []
     soup = BeautifulSoup(resp, 'lxml')
-    main_table = soup.find('table', class_='table table-striped table-bordered').find_all('tr')
-    for tr in main_table:
-        trs = tr.find('td')
-        if trs:
-            proxies_list.append(trs.text)
-    return proxies_list
+    main_div = soup.find('div', class_='list-code http')
+    table = main_div.find('table').text
+    all_tr = table.find_all('tr')
+    for tr in all_tr:
+        proxy = tr.find('td').text
+        print(proxy)
 
-
-def get_session(proxies):
-    session = requests.Session()
-    proxy = random.choice(proxies)
-    session.proxies = {"http": proxy, "https": proxy}
-    return session
-
-
-def validate_proxy(proxies, session):
-    valid_proxies = []
-    for i in range(30):
-        s = get_session(proxies)
-        try:
-            proxy = s.get("http://icanhazip.com", timeout=1.5).text.strip()
-            if proxy and len(proxy) < 70:
-                valid_proxies.append(proxy)
-        except Exception as e:
-            continue
-        return valid_proxies
-
-
-def get_valid_proxies():
-    proxies_list = parse_proxies(get_proxy_html())
-    valid_proxies = validate_proxy(proxies_list, get_session(proxies_list))
-    return valid_proxies
+parse_proxies(get_proxy_html())
+# def get_session(proxies):
+#     session = requests.Session()
+#     proxy = random.choice(proxies)
+#     session.proxies = {"http": proxy, "https": proxy}
+#     return session
+#
+#
+# def validate_proxy(proxies, session):
+#     valid_proxies = []
+#     for proxy in proxies:
+#         s = get_session(proxies)
+#         try:
+#             proxy = s.get("http://icanhazip.com", timeout=1.5).text.strip()
+#             print(proxy)
+#             if proxy and len(proxy) < 16:
+#                 valid_proxies.append(proxy)
+#         except Exception as e:
+#             continue
+#         if valid_proxies:
+#             return valid_proxies
+#
+#
+# def get_valid_proxies():
+#     proxies_list = parse_proxies(get_proxy_html())
+#     valid_proxies = validate_proxy(proxies_list, get_session(proxies_list))
+#     return valid_proxies
+# get_valid_proxies()
--- parser_pleer.ru.py
+++ parser_pleer.ru.py
@@ -7,31 +7,22 @@ URL = 'https://www.pleer.ru/fullcatalog.html/'
 HEADERS = {'User-agent': 'curl/7.64.1'}
 
 
-def get_session(valid_proxies):
+def get_html(valid_proxies, url, headers=''):
     session = requests.Session()
-    if valid_proxies != []:
-        proxy = random.choice(valid_proxies)
-        if len(proxy) != 0:
-            session.proxies = {"http": proxy, "https": proxy}
-            return session
-        else:
-            raise Exception
-
-
-
-def get_html(url, headers):
-    response = requests.get(url=url, headers=headers)
+    proxy = random.choice(valid_proxies)
+    session.proxies = {"http": proxy, "https": proxy}
+    response = session.get(url=url, headers=headers)
+    print(response)
     return response.text
 
 
-
-
-def main_parser(html, valid_proxies, session):
+def main_parser(html):
     """Main parser, parse web shop."""
     soup = BeautifulSoup(html, 'lxml')
     data = []
     HEADER = {
-        'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 YaBrowser/21.8.3.614 Yowser/2.5 Safari/537.36'}
+        'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                      ' Chrome/92.0.4515.159 YaBrowser/21.8.3.614 Yowser/2.5 Safari/537.36'}
 
     main_categories = soup.find_all('div', class_='top-menu-category')
     for category in main_categories:
@@ -40,16 +31,14 @@ def main_parser(html, valid_proxies, session):
                 'main_categories': category.text,
             }
         )
-
-    all_uls = soup.find_all('ul', class_='top-menu-catalog')
-    for ul in all_uls:
-
         with open('products_links.txt', 'r') as file:
-                for url in file:
-                    if "\"" in url.strip():
-                        continue
-                    else:
-                        response2 = get_session(ch_p.get_valid_proxies()).get(url=url)
-                        print(response2)
+            for url in file:
+                url = url.strip()
+                try:
+                    response2 = get_html(ch_p.get_valid_proxies(), URL)
+                    print(response2)
+                except Exception as e:
+                    print(e)
 
-main_parser(get_html(URL, HEADERS), ch_p.get_valid_proxies(), get_session(ch_p.get_valid_proxies()))
+
+main_parser(get_html(ch_p.get_valid_proxies(), URL, HEADERS))
