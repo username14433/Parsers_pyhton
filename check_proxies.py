1e2f0284077ac20b8ad254678a1fd7788d375e63
Vladimir <you@example.com>
2021-11-07 16:19:13 +0300

    'parsing_with_many_proxies'

--- check_proxies.py
+++ check_proxies.py
@@ -6,7 +6,7 @@ URL = 'http://www.w3.org/TR/html4/strict.dtd'
 
 
 def get_proxy_html():
-    URL1 = 'https://openproxy.space/ru/list/890-w2mSkt'
+    URL1 = 'https://free-proxy-list.net/'
     response = requests.get(url=URL1)
     return response.text
 
@@ -15,14 +15,23 @@ def parse_proxies(resp):
     """Parse web site with free proxies."""
     proxies_list = []
     soup = BeautifulSoup(resp, 'lxml')
-    main_div = soup.find('div', class_='list-code http')
-    table = main_div.find('table').text
-    all_tr = table.find_all('tr')
-    for tr in all_tr:
-        proxy = tr.find('td').text
-        print(proxy)
+    main_div = soup.find('div', class_='table-responsive fpl-list')
+    table = main_div.find('table', class_='table table-striped table-bordered')
+    all_trs = table.find_all('tr')
+    for tr in all_trs:
+        if tr:
+            try:
+                all_tds = tr.find_all('td')
+                proxy = all_tds[0].text.strip()
+                port = all_tds[1].text.strip()
+                proxy_and_host = f"{proxy}:{port}"
+                proxies_list.append(proxy_and_host)
+            except IndexError:
+                continue
+    return proxies_list
 
-parse_proxies(get_proxy_html())
+
+
 # def get_session(proxies):
 #     session = requests.Session()
 #     proxy = random.choice(proxies)
@@ -32,21 +41,19 @@ parse_proxies(get_proxy_html())
 #
 # def validate_proxy(proxies, session):
 #     valid_proxies = []
-#     for proxy in proxies:
+#     for proxy in range(10):
 #         s = get_session(proxies)
 #         try:
 #             proxy = s.get("http://icanhazip.com", timeout=1.5).text.strip()
 #             print(proxy)
-#             if proxy and len(proxy) < 16:
+#             if proxy:
 #                 valid_proxies.append(proxy)
 #         except Exception as e:
 #             continue
-#         if valid_proxies:
-#             return valid_proxies
-#
+#         return valid_proxies
 #
 # def get_valid_proxies():
 #     proxies_list = parse_proxies(get_proxy_html())
 #     valid_proxies = validate_proxy(proxies_list, get_session(proxies_list))
-#     return valid_proxies
+#     print(valid_proxies)
 # get_valid_proxies()
--- parser_pleer.ru.py
+++ parser_pleer.ru.py
@@ -7,23 +7,37 @@ URL = 'https://www.pleer.ru/fullcatalog.html/'
 HEADERS = {'User-agent': 'curl/7.64.1'}
 
 
-def get_html(valid_proxies, url, headers=''):
+def get_html(valid_proxies, url, headers=None):
+    if headers is None:
+        headers = {}
     session = requests.Session()
     proxy = random.choice(valid_proxies)
-    session.proxies = {"http": proxy, "https": proxy}
+    if "http" in url:
+        session.proxies = {"http": proxy}
+    else:
+        session.proxies = {"https": proxy}
     response = session.get(url=url, headers=headers)
-    print(response)
     return response.text
+
+
+def get_session(valid_proxies, url, headers):
+    session = requests.Session()
+    proxy = random.choice(valid_proxies)
+    if "http" in url:
+        session.proxies = {"http": proxy}
+    else:
+        session.proxies = {"https": proxy}
+    response2 = session.get(url=url, headers=headers)
+    return response2
 
 
 def main_parser(html):
     """Main parser, parse web shop."""
-    soup = BeautifulSoup(html, 'lxml')
+    HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+                            ' (KHTML, like Gecko) Chrome/93.0.4577.82 YaBrowser/21.9.2.169 Yowser/2.5 Safari/537.36'}
+    print("successful")
     data = []
-    HEADER = {
-        'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                      ' Chrome/92.0.4515.159 YaBrowser/21.8.3.614 Yowser/2.5 Safari/537.36'}
-
+    soup = BeautifulSoup(html, 'lxml')
     main_categories = soup.find_all('div', class_='top-menu-category')
     for category in main_categories:
         data.append(
@@ -31,14 +45,16 @@ def main_parser(html):
                 'main_categories': category.text,
             }
         )
+        print("successful")
         with open('products_links.txt', 'r') as file:
             for url in file:
+
                 url = url.strip()
-                try:
-                    response2 = get_html(ch_p.get_valid_proxies(), URL)
-                    print(response2)
-                except Exception as e:
-                    print(e)
+                if url:
+                    print(url)
+                    response2 = get_session(ch_p.parse_proxies(ch_p.get_proxy_html()), url, HEADER)
+                    print(response2.text)
 
 
-main_parser(get_html(ch_p.get_valid_proxies(), URL, HEADERS))
+
+main_parser(get_html(ch_p.parse_proxies(ch_p.get_proxy_html()), URL, HEADERS))
